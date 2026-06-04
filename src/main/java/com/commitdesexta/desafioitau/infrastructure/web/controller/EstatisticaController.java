69727c1b49f45158e229459b128596b09f481a8c
Commit de sexta <dev@commitdesexta.local>
2026-06-04 11:14:25 -0300

    feat(api): Adiciona controladores iniciais para Estatística e Transação

---
+++ src/main/java/com/commitdesexta/desafioitau/infrastructure/web/controller/EstatisticaController.java
@@ -0,0 +1,7 @@
+package com.commitdesexta.desafioitau.infrastructure.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/estatistica")
+public class EstatisticaController {
+}
